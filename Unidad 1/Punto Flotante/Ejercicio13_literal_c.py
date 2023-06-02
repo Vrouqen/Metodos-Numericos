d72497a58d3f6252380f06f7f09bf30e23b43c90
Vrouqen <luiselomi@outlook.com>
2023-06-01 21:51:11 -0500

    Se sube el ejercicio #13 literal c
    Importamos la librería sys.
    definimos dos variables con el mismo valor.
    Imprimimos la precisión de los tipos de variable.

---
+++ Unidad 1/Punto Flotante/Ejercicio13_literal_c.py
@@ -0,0 +1,8 @@
+import sys
+# Variable de tipo float de 32 bits
+float_32 = 1.2345678901234567890123456789
+# Variable de tipo float de 64 bits
+float_64 = 1.2345678901234567890123456789
+
+print("Precisión del float de 32 bits:", sys.float_info.dig)
+print("Precisión del float de 64 bits:", sys.float_info.dig)
