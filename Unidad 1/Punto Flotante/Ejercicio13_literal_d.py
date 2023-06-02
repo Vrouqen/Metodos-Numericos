6f543104b0e319bd8944e1a3e8ee118c296100ec
Vrouqen <luiselomi@outlook.com>
2023-06-01 21:57:55 -0500

    Se sube el ejercicio #13 - literal d)
    Se crea una variable de tipo int
    Se transforma esa variable a float
    Se retorna esa variable a tipo int
    Se imprimen los resultados

---
+++ Unidad 1/Punto Flotante/Ejercicio13_literal_d.py
@@ -0,0 +1,6 @@
+integer_32 = 1234567890  # Entero de 32 bits
+float_representation = float(integer_32)
+integer_back = int(float_representation)
+print("Entero original:", integer_32)
+print("Representación en float:", float_representation)
+print("Entero recuperado:", integer_back)
