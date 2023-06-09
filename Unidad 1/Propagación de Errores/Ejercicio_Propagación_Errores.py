a87d7dc2a032978f586337d36aa22bc294001907
Vrouqen <luiselomi@outlook.com>
2023-06-08 19:49:08 -0500

    Se agrega gráfico de la función
    Importamos la librería matplotlib
    Definimos dos arreglos uno con valores de "x" y otro de "y".
    Se llenan los arreglos llamando a la función de calcular.
    Se crea el gráfico y se muestra en pantalla.

--- Unidad 1/Propagación de Errores/Ejercicio_Propagación_Errores.py
+++ Unidad 1/Propagación de Errores/Ejercicio_Propagación_Errores.py
@@ -1,4 +1,5 @@
 import math
+import matplotlib.pyplot as plt
 def calcular_expresion(x):
     numerador = math.exp(x)
     denominador = math.exp(x) - 1
@@ -22,3 +23,19 @@ valor = solicitar_valor()
 # Calcular y mostrar el resultado
 resultado = calcular_expresion(valor)
 print("El resultado de la expresión para x = {} es: {}".format(valor, resultado))
+
+#Gráfica de la función
+print()
+valorFinalX = float(input("Ingrese el valor final de X para ver su gráfico: "))
+if(valorFinalX>1):
+    valorX=1
+    valoresY=[]
+    valoresX=[]
+    while valorX<=valorFinalX:
+        valoresX.append([valorX]) #arreglo de valores de x
+        valoresY.append([calcular_expresion(valorX)]) #arreglo de valores en y con respectivo cálculo
+        valorX+=0.1 #Incremento pequeño para mostrar más presición en el gráfico
+    plt.plot(valoresX,valoresY) #Se crea el gráfico
+    plt.show() #Se imprime el gráfico
+else:
+    print("No es válido un número menor a 1")
