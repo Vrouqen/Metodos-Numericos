7727d75b8127138e3c07f6084f034661d084b98b
Vrouqen <luiselomi@outlook.com>
2023-06-26 19:37:56 -0500

    Se corrigen las validaciones de ingreso de datos
    Anteriormente el programa se cerraba al colocar una letra donde correspondía un número.
    Se utilizó una variable booleana para realizar ese control.

--- Proyecto/testeoInterfaz.py
+++ Proyecto/testeoInterfaz.py
@@ -43,52 +43,84 @@ def entrarPropagErrores():
     main.hide()
 
 #Validaciones
-def validadorDecimales(decimalString):
+def validadorEnteros(decimalString):
     try:
-        decimal=float(decimalString)
+        decimal=int(decimalString)
+        validado=True
     except:
         msg = QMessageBox()
         msg.setIcon(QMessageBox.Critical)
         msg.setWindowTitle("Error")
         msg.setText("Ingrese correctamente el número")
         msg.exec_()
+        validado=False
         #Hace falta terminar la ejecucion del metodo
-    return decimal
+    return validado
+
+def validadorDecimales(decimalString):
+    try:
+        decimal=float(decimalString)
+        validado=True
+    except:
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Critical)
+        msg.setWindowTitle("Error")
+        msg.setText("Ingrese correctamente el número")
+        msg.exec_()
+        validado=False
+        #Hace falta terminar la ejecucion del metodo
+    return validado
 
 def validarHexadecimales(numeroHexadecimal):
     try:
         int(numeroHexadecimal, 16)
+        validado=True
     except ValueError:
         msg = QMessageBox()
         msg.setIcon(QMessageBox.Critical)
         msg.setWindowTitle("Error")
         msg.setText("Ingrese correctamente el número hexadecimal")
         msg.exec_()
-    return numeroHexadecimal
+        validado=False
+    return validado
 
 def validarOctales(numeroOctal):
     try:
         int(numeroOctal, 8)
+        validado=True
     except ValueError:
         msg = QMessageBox()
         msg.setIcon(QMessageBox.Critical)
         msg.setWindowTitle("Error")
         msg.setText("Ingrese correctamente el número octal")
         msg.exec_()
-    return numeroOctal
+        validado=False
+    return validado
 
 #Funciones/utilidades
 def calcularErrorAbs():
-    valorReal=validadorDecimales(calcErrores.textReal.toPlainText())
-    valorAproximado=validadorDecimales(calcErrores.textAprox.toPlainText())
-    errorAbsoluto=Unidad1.calcularErrorAbsoluto(valorReal,valorAproximado)
-    calcErrores.labelResultado.setText("Error absoluto: "+str(errorAbsoluto))
+    correcto1=validadorDecimales(calcErrores.textReal.toPlainText())
+    correcto2=validadorDecimales(calcErrores.textAprox.toPlainText())
+    if correcto1==True and correcto2==True:
+        valorReal=float(calcErrores.textReal.toPlainText())
+        valorAproximado=float(calcErrores.textAprox.toPlainText())
+        errorAbsoluto=Unidad1.calcularErrorAbsoluto(valorReal,valorAproximado)
+        calcErrores.labelResultado.setText("Error absoluto: "+str(errorAbsoluto))
+    else:
+        calcErrores.textReal.setText("")
+        calcErrores.textAprox.setText("")
 
 def calcularErrorRelativo():
-    valorReal=validadorDecimales(calcErrores.textReal.toPlainText())
-    valorAproximado=validadorDecimales(calcErrores.textAprox.toPlainText())
-    errorRelativo=Unidad1.calcularErrorRelativo(valorReal,valorAproximado)
-    calcErrores.labelResultado.setText("Error relativo: "+str(errorRelativo))
+    correcto1=validadorDecimales(calcErrores.textReal.toPlainText())
+    correcto2=validadorDecimales(calcErrores.textAprox.toPlainText())
+    if correcto1==True and correcto2==True:
+        valorReal=float(calcErrores.textReal.toPlainText())
+        valorAproximado=float(calcErrores.textAprox.toPlainText())
+        errorRelativo=Unidad1.calcularErrorRelativo(valorReal,valorAproximado)
+        calcErrores.labelResultado.setText("Error relativo: "+str(errorRelativo))
+    else:
+        calcErrores.textReal.setText("")
+        calcErrores.textAprox.setText("")
 
 def transformarBinarioToDecimal():
     cadena=sistNumeros.textNumero.toPlainText()
@@ -107,67 +139,88 @@ def transformarBinarioToDecimal():
         msg.setWindowTitle("Error")
         msg.setText("Unicamente es válido el ingreso de 0 y 1")
         msg.exec_()
+        sistNumeros.textNumero.setText("")
     else:
         decimal=Unidad1.convertirBinarioToDecimal(cadena)
         sistNumeros.labelNumTransformado.setText(str(decimal))
 
 def transformarDecimalToBinario():
-    decimal=int(validadorDecimales(sistNumeros.textNumero.toPlainText()))
-    binario=Unidad1.convertirDecimalToBinario(decimal) #Solo transforma números enteros
-    sistNumeros.labelNumTransformado.setText(str(binario))
+    if validadorEnteros(sistNumeros.textNumero.toPlainText()):
+        decimal=int(sistNumeros.textNumero.toPlainText())
+        binario=Unidad1.convertirDecimalToBinario(decimal) #Solo transforma números enteros
+        sistNumeros.labelNumTransformado.setText(str(binario))
+    else:
+        sistNumeros.textNumero.setText("")
 
 def transfomarHexadecimalToDecimal():
-    hexadecimal=validarHexadecimales(sistNumeros.textNumero.toPlainText())
-    decimal=Unidad1.convertirHexadecimalToDecimal(hexadecimal)
-    sistNumeros.labelNumTransformado.setText(str(decimal))
+    if validadorEnteros(sistNumeros.textNumero.toPlainText()):
+        hexadecimal=int(sistNumeros.textNumero.toPlainText())
+        decimal=Unidad1.convertirHexadecimalToDecimal(hexadecimal)
+        sistNumeros.labelNumTransformado.setText(str(decimal))
+    else:
+        sistNumeros.textNumero.setText("")
 
 def transformarOctalToDecimal():
-    octal=int(validarOctales(sistNumeros.textNumero.toPlainText()))
-    decimal=Unidad1.convertirOctalToDecimal(octal)
-    sistNumeros.labelNumTransformado.setText(str(decimal))
+    if validadorEnteros(sistNumeros.textNumero.toPlainText()):
+        octal = int(sistNumeros.textNumero.toPlainText())
+        decimal=Unidad1.convertirOctalToDecimal(octal)
+        sistNumeros.labelNumTransformado.setText(str(decimal))
+    else:
+        sistNumeros.textNumero.setText("")
 
 def solucionDeEcuacion():
-    valorX = float(validadorDecimales(propagErrores.textValorX.toPlainText()))
-    if valorX > 0:
-        if len(str(valorX)) <= 6:  # Verificar si el valor tiene hasta 6 dígitos
-            solucion = Unidad1.solucionDeEcuacion(valorX)
-            propagErrores.labelResultado.setText(str(solucion))
+    if validadorDecimales(propagErrores.textValorX.toPlainText()):
+        valorX = float(propagErrores.textValorX.toPlainText())
+        if valorX > 0:
+            if len(str(valorX)) <= 6:  # Verificar si el valor tiene hasta 6 dígitos
+                solucion = Unidad1.solucionDeEcuacion(valorX)
+                propagErrores.labelResultado.setText(str(solucion))
+            else:
+                msg = QMessageBox()
+                msg.setIcon(QMessageBox.Critical)
+                msg.setWindowTitle("Error")
+                msg.setText("Solo se aceptan números de hasta 6 dígitos")
+                msg.exec_()
+                propagErrores.textValorX.setText("")
         else:
             msg = QMessageBox()
             msg.setIcon(QMessageBox.Critical)
             msg.setWindowTitle("Error")
-            msg.setText("Solo se aceptan números de hasta 6 dígitos")
+            msg.setText("Solo se aceptan números mayores a 0")
             msg.exec_()
             propagErrores.textValorX.setText("")
     else:
-        msg = QMessageBox()
-        msg.setIcon(QMessageBox.Critical)
-        msg.setWindowTitle("Error")
-        msg.setText("Solo se aceptan números mayores a 0")
-        msg.exec_()
         propagErrores.textValorX.setText("")
 
 def mostraGrafica():
-    valorX=float(validadorDecimales(propagErrores.textValorX.toPlainText()))
-    if valorX>0:
-        Unidad1.graficaDeEcuacion(valorX)
+    if validadorDecimales(propagErrores.textValorX.toPlainText()):
+        valorX=float(propagErrores.textValorX.toPlainText())
+        if valorX>0:
+            Unidad1.graficaDeEcuacion(valorX)
+        else:
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Critical)
+            msg.setWindowTitle("Error")
+            msg.setText("Solo se aceptan numeros mayores a 0")
+            msg.exec_()
+            propagErrores.textValorX.setText("")
     else:
-        msg = QMessageBox()
-        msg.setIcon(QMessageBox.Critical)
-        msg.setWindowTitle("Error")
-        msg.setText("Solo se aceptan numeros mayores a 0")
-        msg.exec_()
         propagErrores.textValorX.setText("")
 
 def convertirIEE754_32bits():
-    num=float(validadorDecimales(sistNumerosIEEE.textNumero.toPlainText()))
-    convertido=Unidad1.convertirIEE754_32bits(num)
-    sistNumerosIEEE.labelNumTransformado.setText(str(convertido))
-
+    if validadorDecimales(sistNumerosIEEE.textNumero.toPlainText()):
+        num=float(sistNumerosIEEE.textNumero.toPlainText())
+        convertido=Unidad1.convertirIEE754_32bits(num)
+        sistNumerosIEEE.labelNumTransformado.setText(str(convertido))
+    else:
+        sistNumerosIEEE.textNumero.setText("")
 def convertirIEE754_64bits():
-    num=float(validadorDecimales(sistNumerosIEEE.textNumero.toPlainText()))
-    convertido=Unidad1.convertirIEE754_64bits(num)
-    sistNumerosIEEE.labelNumTransformado.setText(str(convertido))
+    if validadorDecimales(sistNumerosIEEE.textNumero.toPlainText()):
+        num=float(sistNumerosIEEE.textNumero.toPlainText())
+        convertido=Unidad1.convertirIEE754_64bits(num)
+        sistNumerosIEEE.labelNumTransformado.setText(str(convertido))
+    else:
+        sistNumerosIEEE.textNumero.setText("")
 
 #Asignación de funciones a botones de manejo de interfaz
 main.botonCalError.clicked.connect(entrarCalcError) #Entrar a cálculo de errores
