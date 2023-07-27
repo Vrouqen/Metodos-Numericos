b7e9d1eac9fea56e21e3874744905a70969b1d6b
Vrouqen <luiselomi@outlook.com>
2023-07-27 17:45:48 -0500

    Se agregan métodos de series de Taylor
    Se crea un método para calcular el factorial.
    Se crea un método para permitir el ingreso de funciones.
    Se crea un método para calcular la derivada enésima.
    Se crea un método de series de Taylor que llama al resto de métodos.

--- Proyecto/unidades/Unidad_2.py
+++ Proyecto/unidades/Unidad_2.py
@@ -1,1 +1,26 @@
 
+class Unidad2:
+    #TEMA 1 - Series de Taylor
+    def factorial(n):
+        # Calcula el factorial de un número entero n
+        if n == 0:
+            return 1
+        else:
+            return n * Unidad2.factorial(n - 1)
+    def transformarAFuncion(x, funcionStr):
+        try:
+            return eval(funcionStr)
+        except:
+            print("Ecuación mal ingresada")
+
+    def derivadaEnesima(funcionStr, n, x):
+        # Calcula la n-ésima derivada de la función en el punto x
+        h = 1e-5  # Un valor pequeño para el cálculo numérico de la derivada
+        return (Unidad2.transformarAFuncion(x + h*n,funcionStr) - Unidad2.transformarAFuncion(x,funcionStr))/ (h ** n)
+
+    def calcularSeriesTaylor(funcionStr, punto, x, n):
+        # Calcula la aproximación de la función en el punto utilizando n términos de la serie de Taylor
+        aproximacion = 0
+        for i in range(n + 1):
+            aproximacion += Unidad2.derivadaEnesima(funcionStr, i, punto) * (x - punto) ** i / Unidad2.factorial(i)
+        return aproximacion
