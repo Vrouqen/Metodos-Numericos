681b81937a82025ec3b2a651b872ddf2c1edc0d7
Allan <allancorrea2002@outlook.es>
2023-08-09 18:23:59 -0500

    Interfaz avance
    Asignacion de temas de la unidad 1 con funcionalidad

--- Proyecto/interfaz_test_pepa.py
+++ Proyecto/interfaz_test_pepa.py
@@ -141,12 +141,152 @@ def mostraGraficaPropagacionErrores():
     else:
         main.textValorX.setText("")
 
+def convertirIEE754_32bits():
+    if validadorDecimales(main.textNumero_2.toPlainText()):
+        num=float(main.textNumero_2.toPlainText())
+        convertido=Unidad1.convertirIEE754_32bits(num)
+        main.labelNumTransformado_2.setText(str(convertido))
+    else:
+        main.textNumero.setText("")
+def convertirIEE754_64bits():
+    if validadorDecimales(main.textNumero_2.toPlainText()):
+        num=float(main.textNumero_2.toPlainText())
+        convertido=Unidad1.convertirIEE754_64bits(num)
+        main.labelNumTransformado_2.setText(str(convertido))
+    else:
+        main.textNumero.setText("")
+
+def calcularTeoremaBolzano():
+    if validadorDecimales(main.textValorInf.toPlainText()) and validadorDecimales(main.textValorSup.toPlainText()):
+        valorInf=float(main.textValorInf.toPlainText())
+        valorSup=float(main.textValorSup.toPlainText())
+        if valorInf < valorSup:
+            funcion=main.textFuncion.toPlainText()
+            respuesta=Unidad1.teoremaBolzano(valorInf,valorSup,funcion)
+            if respuesta==True:
+                main.labelResultado_3.setText("Existe una raíz en el intervalo dado")
+            else:
+                main.labelResultado_3.setText("No se encontró una raíz en el intervalo dado")
+        else:
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Critical)
+            msg.setWindowTitle("Error")
+            msg.setText("Ingrese correctamente el intervalo")
+            msg.exec_()
+            main.textValorInf.setText("")
+            main.textValorSup.setText("")
+    else:
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Critical)
+        msg.setWindowTitle("Error")
+        msg.setText("Ingrese correctamente los números")
+        msg.exec_()
+        main.textValorInf.setText("")
+        main.textValorSup.setText("")
+
+def mostrarGraficaFuncionBolzano():
+    if validadorDecimales(main.textValorInf.toPlainText()) and validadorDecimales(main.textValorSup.toPlainText()):
+        valorInf = float(main.textValorInf.toPlainText())
+        valorSup = float(main.textValorSup.toPlainText())
+        if valorInf < valorSup:
+            funcion = main.textFuncion.toPlainText()
+            Unidad1.graficarFuncion(valorInf,valorSup,funcion)
+        else:
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Critical)
+            msg.setWindowTitle("Error")
+            msg.setText("Ingrese correctamente el intervalo")
+            msg.exec_()
+            main.textValorInf.setText("")
+            main.textValorSup.setText("")
+    else:
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Critical)
+        msg.setWindowTitle("Error")
+        msg.setText("Ingrese correctamente los números")
+        msg.exec_()
+        main.textValorInf.setText("")
+        main.textValorSup.setText("")
+
+def mostrarGraficaFuncionBiseccion():
+    validarInf=validadorDecimales(main.textValorInf_2.toPlainText())
+    validarSup=validadorDecimales(main.textEdit.toPlainText())
+    if validarInf==True and validarSup==True:
+        valorInf = float(main.textValorInf_2.toPlainText())
+        valorSup = float(main.textEdit.toPlainText())
+        if valorInf < valorSup:
+            funcion = main.textFuncion_2.toPlainText()
+            Unidad1.graficarFuncion(valorInf,valorSup,funcion)
+        else:
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Critical)
+            msg.setWindowTitle("Error")
+            msg.setText("Ingrese correctamente el intervalo")
+            msg.exec_()
+            main.textValorInf_2.setText("")
+            main.textEdit.setText("")
+    else:
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Critical)
+        msg.setWindowTitle("Error")
+        msg.setText("Ingrese correctamente los números")
+        msg.exec_()
+        main.textValorInf_2.setText("")
+        main.textEdit.setText("")
+
+def calcularMétodoBisección():
+    print(validadorDecimales(main.textValorInf_2.toPlainText()))
+    print(validadorDecimales(main.textEdit.toPlainText()))
+    if validadorDecimales(main.textValorInf_2.toPlainText()) and validadorDecimales(main.textEdit.toPlainText()):
+        valorInf=float(main.textValorInf_2.toPlainText())
+        valorSup=float(main.textEdit.toPlainText())
+        if valorInf < valorSup:
+            funcion=main.textFuncion_2.toPlainText()
+            respuesta=Unidad1.metodoBiseccion(valorInf,valorSup,0.0001,funcion)
+            main.labelResultado_4.setText("La raíz en el intervalo es "+str(respuesta))
+        else:
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Critical)
+            msg.setWindowTitle("Error")
+            msg.setText("Ingrese correctamente el intervalo")
+            msg.exec_()
+            main.textValorInf_2.setText("")
+            main.textValorSup_2.setText("")
+    else:
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Critical)
+        msg.setWindowTitle("Error")
+        msg.setText("Ingrese correctamente los números")
+        msg.exec_()
+        main.textValorInf_2.setText("")
+        main.textEdit.setText("")
+
+def ayuda():
+    msg = QMessageBox()
+    msg.setIcon(QMessageBox.Critical)
+    msg.setWindowTitle("Ayuda")
+    msg.setText("Un ejemplo para insertar una ecuacion de buena forma es: (x**(3+2x))/x+3x**4")
+    msg.exec_()
+    main.textValorInf.setText("")
+    main.textEdit.setText("")
+
 #Asignación de funciones/utilidades
 main.botonAbs.clicked.connect(calcularErrorAbs) #Calcular Error Absoluto
 main.botonRelat.clicked.connect(calcularErrorRelativo) #Calcular Error Relativo
 
 main.botonCalcular.clicked.connect(solucionDeEcuacion)#Solución de la ecuación (e^x)/(e^x -1)
 main.botonGrafico.clicked.connect(mostraGraficaPropagacionErrores)#Grafica de la funcion
+
+main.boton32bits.clicked.connect(convertirIEE754_32bits)#Conversion de numero a IEE 754 32 bits
+main.boton64bits.clicked.connect(convertirIEE754_64bits)#Conversion de numero a IEE 754 64 bits
+
+main.botonCalcular_2.clicked.connect(calcularTeoremaBolzano)#Calcula el Teorema de bolzano
+main.botonGrafico_2.clicked.connect(mostrarGraficaFuncionBolzano)#Grafica del Teoreama de bolzano
+main.botonAyuda.clicked.connect(ayuda)#Boton de ayuda para ingresar la función
+
+main.botonCalcular_3.clicked.connect(calcularMétodoBisección)#Calcula el método de Bisección
+main.botonGrafico_3.clicked.connect(mostrarGraficaFuncionBiseccion)#Grafica del método de Bisección
+main.botonAyuda_2.clicked.connect(ayuda)#Boton de ayuda para ingresar la función
 
 main.show()
 app.exec()
