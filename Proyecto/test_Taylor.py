008895917a798315bb67384dc936a0936cae5955
Vrouqen <luiselomi@outlook.com>
2023-09-01 00:56:00 -0500

    Corrección en series de Taylor
    Se modificó el método aplicado para resolver este tipo de ejercicios.

--- Proyecto/test_Taylor.py
+++ Proyecto/test_Taylor.py
@@ -15,33 +15,20 @@ taylor=uic.loadUi("interfaz\SeriesTaylor.ui")
 
 def resolver():
     try:
+        funcionStr=taylor.textFuncion.toPlainText()
         valorX=float(taylor.textValorX.toPlainText())
-        try:
-            nDerivadas=int(taylor.textNDerivadas.toPlainText())
-            try:
-                funcionStr=taylor.textFuncion.toPlainText()
-                resultado=Unidad2.calcularSeriesTaylor(funcionStr,0,valorX,nDerivadas)
-                print(resultado)
-            except:
-                msg = QMessageBox()
-                msg.setIcon(QMessageBox.Critical)
-                msg.setWindowTitle("Error")
-                msg.setText("Ingrese correctamente la función")
-                msg.exec_()
-        except:
-                msg = QMessageBox()
-                msg.setIcon(QMessageBox.Critical)
-                msg.setWindowTitle("Error")
-                msg.setText("Ingrese correctamente la\ncantidad de derivadas")
-                msg.exec_()
-                taylor.textNDerivadas.setText("")
+        nDerivadas=int(taylor.textNDerivadas.toPlainText())
+        resultado=Unidad2.seriesDeTaylor(funcionStr,valorX,nDerivadas)
+        print(resultado)
+        taylor.labelResultado.setText("El resultado es : "+str(resultado))
+        taylor.textValorX.setText("")
+        
     except:
         msg = QMessageBox()
         msg.setIcon(QMessageBox.Critical)
         msg.setWindowTitle("Error")
-        msg.setText("Ingrese correctamente el valor de x ")
+        msg.setText("Algún campo está mal digitado")
         msg.exec_()
-        taylor.textValorX.setText("")
 
 taylor.botonResolver.clicked.connect(resolver)
 
--- Proyecto/unidades/Unidad_2.py
+++ Proyecto/unidades/Unidad_2.py
@@ -1,26 +1,22 @@
 
 class Unidad2:
     #TEMA 1 - Series de Taylor
-    def factorial(n):
-        # Calcula el factorial de un número entero n
-        if n == 0:
-            return 1
-        else:
-            return n * Unidad2.factorial(n - 1)
+
     def transformarAFuncion(x, funcionStr):
         try:
             return eval(funcionStr)
         except:
             print("Ecuación mal ingresada")
 
-    def derivadaEnesima(funcionStr, n, x):
-        # Calcula la n-ésima derivada de la función en el punto x
-        h = 1e-5  # Un valor pequeño para el cálculo numérico de la derivada
-        return (Unidad2.transformarAFuncion(x + h*n,funcionStr) - Unidad2.transformarAFuncion(x,funcionStr))/ (h ** n)
+    def seriesDeTaylor(funcionStr, punto, orden):
+        resultado = 0
+        for n in range(orden + 1):
+            termino = Unidad2.transformarAFuncion(punto,funcionStr) * (punto - 0) ** n / Unidad2.factorial(n)
+            resultado += termino
+        return resultado
 
-    def calcularSeriesTaylor(funcionStr, punto, x, n):
-        # Calcula la aproximación de la función en el punto utilizando n términos de la serie de Taylor
-        aproximacion = 0
-        for i in range(n + 1):
-            aproximacion += Unidad2.derivadaEnesima(funcionStr, i, punto) * (x - punto) ** i / Unidad2.factorial(i)
-        return aproximacion
+    def factorial(n):
+        if n == 0:
+            return 1
+        else:
+            return n * Unidad2.factorial(n - 1)
